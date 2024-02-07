d93ff9d9868f9c089d733a956a0fd24c2d8da3ff
Jonathan Maronde <j2mike4@gmail.com>
2024-02-07 09:57:20 -0600

    Refactored palindrome check to use a function

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,8 +1,10 @@
 # Checks if string is a palindrome
 
-text = "did"
+def check_palindrome(text):
+    if text == text[::-1]:
+        print(f'{text} is a palindrome.')
+    else:
+        print(f'{text} is not a palindrome.')
 
-if text == text[::-1]:
-   print(f"{text} is a palindrome")
-else:
-   print(f"{text} is not a palindrome")
+text = "did"
+check_palindrome(text)
